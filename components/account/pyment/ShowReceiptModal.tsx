892f001fafc21e500ebc23ddf1bab62ff9a74a8f
Arrivas <lolhacker121@gmail.com>
2023-06-12 13:23:33 +0800

    added fix add payment

--- components/account/pyment/ShowReceiptModal.tsx
+++ components/account/pyment/ShowReceiptModal.tsx
@@ -21,7 +21,10 @@ const ShowReceiptModal: React.FC<ShowReceiptModalProps> = ({
       >
         <div className="flex items-center justify-center min-h-screen px-4 text-center">
           <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-          <button onClick={() => setShowReceipt(false)}></button>
+          <button
+            className="hidden"
+            onClick={() => setShowReceipt(false)}
+          ></button>
           <div className="inline-block p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-lg">
             <div className="my-2">
               <img

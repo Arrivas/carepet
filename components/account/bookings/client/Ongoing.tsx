3e91671f1a7bdda30a504664d2bfc72842928fc4
Arrivas <lolhacker121@gmail.com>
2023-06-12 12:26:38 +0800

    fix chat

--- components/account/bookings/client/Ongoing.tsx
+++ components/account/bookings/client/Ongoing.tsx
@@ -333,174 +333,127 @@ const Ongoing: React.FC<OngoingProps> = ({}) => {
   return (
     <>
       <div className="overflow-y-auto max-h-[99%]">
-        {bookings
-          ?.filter((item: any) => {
-            if (
-              item.status === "pendingPayment" ||
-              item.status === "ongoing" ||
-              item.status === "paymentSubmitted" ||
-              item.status === "paymentRejected"
-            ) {
-              // @ts-ignore
-              return user?.bookedDocIds?.includes(item?.docId);
-            }
-            return false;
-          })
-          .map((item: any) => {
-            const {
-              bookingDetails,
-              clientDetails,
-              providerInfo,
-              scheduling,
-              docId,
-              status,
-              paymentImgUrl,
-            } = item;
+        {bookings?.length !== 0 ? (
+          bookings
+            ?.filter((item: any) => {
+              if (
+                item.status === "pendingPayment" ||
+                item.status === "ongoing" ||
+                item.status === "paymentSubmitted" ||
+                item.status === "paymentRejected"
+              ) {
+                // @ts-ignore
+                return user?.bookedDocIds?.includes(item?.docId);
+              }
+              return false;
+            })
+            .map((item: any) => {
+              const {
+                bookingDetails,
+                clientDetails,
+                providerInfo,
+                scheduling,
+                docId,
+                status,
+                paymentImgUrl,
+              } = item;
 
-            const { clientDocId, clientBookingId } = clientDetails;
-            const { providerDocId, providerName, providerPhone } = providerInfo;
+              const { clientDocId, clientBookingId } = clientDetails;
+              const { providerDocId, providerName, providerPhone } =
+                providerInfo;
 
-            const {
-              description,
-              imgUrl,
-              longDescription,
-              price,
-              serviceName,
-              serviceDocId,
-              serviceProviderName,
-            } = bookingDetails;
+              const {
+                description,
+                imgUrl,
+                longDescription,
+                price,
+                serviceName,
+                serviceDocId,
+                serviceProviderName,
+              } = bookingDetails;
 
-            const { day, time, serviceBookingId } = scheduling;
-            const mergedDateTime = moment(time, "hh:mm A").format("LLLL");
-            return (
-              <div className="p-4" key={docId}>
-                <div className="flex flex-row justify-between items-center">
-                  <img
-                    src={imgUrl}
-                    alt="image"
-                    className="h-[180px] w-[220px] rounded-md object-cover hidden md:block"
-                  />
-                  <div className="flex-grow px-5">
-                    <div className="flex justify-between w-full">
-                      <span>Ref Number</span>
-                      <span className="flex items-center">
-                        <CopyToClipboardButton text={docId} />
-                        {docId}
-                      </span>
+              const { day, time, serviceBookingId } = scheduling;
+              const mergedDateTime = moment(time, "hh:mm A").format("LLLL");
+              return (
+                <div className="p-4" key={docId}>
+                  <div className="flex flex-row justify-between items-center">
+                    <img
+                      src={imgUrl}
+                      alt="image"
+                      className="h-[180px] w-[220px] rounded-md object-cover hidden md:block"
+                    />
+                    <div className="flex-grow px-5">
+                      <div className="flex justify-between w-full">
+                        <span>Ref Number</span>
+                        <span className="flex items-center">
+                          <CopyToClipboardButton text={docId} />
+                          {docId}
+                        </span>
+                      </div>
+                      <div className="flex justify-between w-full my-2">
+                        <span>Service Name</span>
+                        <span>{serviceName}</span>
+                      </div>
+                      <div className="flex justify-between w-full my-2">
+                        <span>Service Provider Name</span>
+                        <span>{serviceProviderName}</span>
+                      </div>
+                      <div className="flex justify-between w-full my-2">
+                        <span>Price</span>
+                        <span>{formatAsCurrency(Number(price))}</span>
+                      </div>
+                      <div className="flex justify-between w-full my-2">
+                        <span>Reserved Date&Time</span>
+                        <span>{mergedDateTime}</span>
+                      </div>
+                      <div className="flex justify-between w-full my-2">
+                        <span>Status</span>
+                        <span>
+                          {status === "pendingPayment"
+                            ? "Pending Payment"
+                            : status === "cancelled"
+                            ? "Cancelled"
+                            : status === "paymentSubmitted"
+                            ? "Payment submitted, waiting for confirmation"
+                            : status === "paymentRejected"
+                            ? "Payment Rejected"
+                            : "Ongoing"}
+                        </span>
+                      </div>
+                      {user?.userType === "Pet-Care Provider" &&
+                        status === "paymentSubmitted" && (
+                          <div className="flex justify-between w-full my-2">
+                            <span>Payment Receipt</span>
+                            <button
+                              onClick={() => setShowReceipt(true)}
+                              className="underline text-blue-400"
+                            >
+                              Show Receipt
+                            </button>
+                          </div>
+                        )}
                     </div>
-                    <div className="flex justify-between w-full my-2">
-                      <span>Service Name</span>
-                      <span>{serviceName}</span>
-                    </div>
-                    <div className="flex justify-between w-full my-2">
-                      <span>Service Provider Name</span>
-                      <span>{serviceProviderName}</span>
-                    </div>
-                    <div className="flex justify-between w-full my-2">
-                      <span>Price</span>
-                      <span>{formatAsCurrency(Number(price))}</span>
-                    </div>
-                    <div className="flex justify-between w-full my-2">
-                      <span>Reserved Date&Time</span>
-                      <span>{mergedDateTime}</span>
-                    </div>
-                    <div className="flex justify-between w-full my-2">
-                      <span>Status</span>
-                      <span>
-                        {status === "pendingPayment"
-                          ? "Pending Payment"
-                          : status === "cancelled"
-                          ? "Cancelled"
-                          : status === "paymentSubmitted"
-                          ? "Payment submitted, waiting for confirmation"
-                          : status === "paymentRejected"
-                          ? "Payment Rejected"
-                          : "Ongoing"}
-                      </span>
-                    </div>
-                    {user?.userType === "Pet-Care Provider" &&
-                      status === "paymentSubmitted" && (
-                        <div className="flex justify-between w-full my-2">
-                          <span>Payment Receipt</span>
-                          <button
-                            onClick={() => setShowReceipt(true)}
-                            className="underline text-blue-400"
-                          >
-                            Show Receipt
-                          </button>
-                        </div>
-                      )}
                   </div>
-                </div>
 
-                <div className="h-full w-full my-2 flex flex-row gap-2 justify-end ">
-                  {(user?.userType === "Client" &&
-                    status === "pendingPayment") ||
-                  (user?.userType === "Client" &&
-                    status === "paymentRejected") ? (
-                    <button
-                      onClick={() => setIsOpen(!isOpen)}
-                      className="text-white flex items-center flex-row bg-green-550 hover:bg-green-600 px-5 py-3 min-w-[120px] rounded-md"
-                    >
-                      Add Payment
-                    </button>
-                  ) : null}
-                  {(user?.userType === "Pet-Care Provider" &&
-                    status === "pendingPayment") ||
-                  (user?.userType === "Pet-Care Provider" &&
-                    status === "paymentRejected") ? (
-                    <button
-                      onClick={() =>
-                        handleCancelBooking(
-                          clientDocId,
-                          clientBookingId,
-                          providerDocId,
-                          serviceDocId,
-                          serviceBookingId
-                        )
-                      }
-                      className="text-white flex items-center flex-row bg-red-500 hover:bg-red-600 px-5 py-3 min-w-[120px] rounded-md"
-                    >
-                      Cancel Booking
-                    </button>
-                  ) : null}
-
-                  {user?.userType === "Pet-Care Provider" &&
-                    status === "paymentSubmitted" && (
-                      <>
-                        <button
-                          onClick={() =>
-                            handleAcceptPayment(
-                              providerDocId,
-                              serviceBookingId,
-                              clientDocId,
-                              clientBookingId
-                            )
-                          }
-                          className="text-white flex items-center flex-row bg-green-500 hover:bg-green-600 px-5 py-3 min-w-[120px] rounded-md"
-                        >
-                          Accept Payment
-                        </button>
-                        <button
-                          onClick={() =>
-                            handleRejectPayment(
-                              providerDocId,
-                              serviceBookingId,
-                              clientDocId,
-                              clientBookingId
-                            )
-                          }
-                          className="text-white flex items-center flex-row bg-red-500 hover:bg-red-600 px-5 py-3 min-w-[120px] rounded-md"
-                        >
-                          Reject Payment
-                        </button>
-                      </>
-                    )}
-                  {user?.userType === "Pet-Care Provider" &&
-                    status === "ongoing" && (
+                  <div className="h-full w-full my-2 flex flex-row gap-2 justify-end ">
+                    {(user?.userType === "Client" &&
+                      status === "pendingPayment") ||
+                    (user?.userType === "Client" &&
+                      status === "paymentRejected") ? (
+                      <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="text-white flex items-center flex-row bg-green-550 hover:bg-green-600 px-5 py-3 min-w-[120px] rounded-md"
+                      >
+                        Add Payment
+                      </button>
+                    ) : null}
+                    {(user?.userType === "Pet-Care Provider" &&
+                      status === "pendingPayment") ||
+                    (user?.userType === "Pet-Care Provider" &&
+                      status === "paymentRejected") ? (
                       <button
                         onClick={() =>
-                          handleCompleteBooking(
+                          handleCancelBooking(
                             clientDocId,
                             clientBookingId,
                             providerDocId,
@@ -508,46 +461,98 @@ const Ongoing: React.FC<OngoingProps> = ({}) => {
                             serviceBookingId
                           )
                         }
-                        className="text-white flex items-center flex-row bg-green-550 hover:bg-green-600 px-5 py-3 min-w-[120px] rounded-md "
+                        className="text-white flex items-center flex-row bg-red-500 hover:bg-red-600 px-5 py-3 min-w-[120px] rounded-md"
                       >
-                        Complete Booking
+                        Cancel Booking
                       </button>
-                    )}
+                    ) : null}
 
-                  <button
-                    onClick={() => router.push(`/messages/room/${docId}`)}
-                    className="text-white flex items-center flex-row bg-blue-500 hover:bg-blue-600 px-5 py-3 min-w-[120px] rounded-md "
-                  >
-                    Send Message
-                  </button>
+                    {user?.userType === "Pet-Care Provider" &&
+                      status === "paymentSubmitted" && (
+                        <>
+                          <button
+                            onClick={() =>
+                              handleAcceptPayment(
+                                providerDocId,
+                                serviceBookingId,
+                                clientDocId,
+                                clientBookingId
+                              )
+                            }
+                            className="text-white flex items-center flex-row bg-green-500 hover:bg-green-600 px-5 py-3 min-w-[120px] rounded-md"
+                          >
+                            Accept Payment
+                          </button>
+                          <button
+                            onClick={() =>
+                              handleRejectPayment(
+                                providerDocId,
+                                serviceBookingId,
+                                clientDocId,
+                                clientBookingId
+                              )
+                            }
+                            className="text-white flex items-center flex-row bg-red-500 hover:bg-red-600 px-5 py-3 min-w-[120px] rounded-md"
+                          >
+                            Reject Payment
+                          </button>
+                        </>
+                      )}
+                    {user?.userType === "Pet-Care Provider" &&
+                      status === "ongoing" && (
+                        <button
+                          onClick={() =>
+                            handleCompleteBooking(
+                              clientDocId,
+                              clientBookingId,
+                              providerDocId,
+                              serviceDocId,
+                              serviceBookingId
+                            )
+                          }
+                          className="text-white flex items-center flex-row bg-green-550 hover:bg-green-600 px-5 py-3 min-w-[120px] rounded-md "
+                        >
+                          Complete Booking
+                        </button>
+                      )}
+
+                    <button
+                      onClick={() => router.push(`/messages/room/${docId}`)}
+                      className="text-white flex items-center flex-row bg-blue-500 hover:bg-blue-600 px-5 py-3 min-w-[120px] rounded-md "
+                    >
+                      Send Message
+                    </button>
+                  </div>
+
+                  {/* separator */}
+                  <div className="w-full h-[1px] my-5 bg-gray-200" />
+                  <ShowReceiptModal
+                    paymentImgUrl={paymentImgUrl}
+                    setShowReceipt={setShowReceipt}
+                    showReceipt={showReceipt}
+                  />
+                  <PaymentModal
+                    price={price}
+                    fileInputRef={fileInputRef}
+                    isOpen={isOpen}
+                    setIsOpen={setIsOpen}
+                    receiptImage={receiptImage}
+                    setReceiptImage={setReceiptImage}
+                    providerName={providerName}
+                    providerPhone={providerPhone}
+                    handleSubmit={handleAddPayment}
+                    clientDocId={clientDocId}
+                    clientBookingId={clientBookingId}
+                    providerDocId={providerDocId}
+                    serviceDocId={serviceDocId}
+                    docId={serviceBookingId}
+                  />
                 </div>
-
-                {/* separator */}
-                <div className="w-full h-[1px] my-5 bg-gray-200" />
-                <ShowReceiptModal
-                  paymentImgUrl={paymentImgUrl}
-                  setShowReceipt={setShowReceipt}
-                  showReceipt={showReceipt}
-                />
-                <PaymentModal
-                  price={price}
-                  fileInputRef={fileInputRef}
-                  isOpen={isOpen}
-                  setIsOpen={setIsOpen}
-                  receiptImage={receiptImage}
-                  setReceiptImage={setReceiptImage}
-                  providerName={providerName}
-                  providerPhone={providerPhone}
-                  handleSubmit={handleAddPayment}
-                  clientDocId={clientDocId}
-                  clientBookingId={clientBookingId}
-                  providerDocId={providerDocId}
-                  serviceDocId={serviceDocId}
-                  docId={serviceBookingId}
-                />
-              </div>
-            );
-          })}
+              );
+            })
+        ) : (
+          <></>
+        )}
       </div>
     </>
   );

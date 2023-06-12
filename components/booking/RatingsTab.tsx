37585bbabb6a020f40c4bad95377e98fddadae44
Arrivas <lolhacker121@gmail.com>
2023-06-12 21:24:33 +0800

    added no ratings on ratings per service

--- components/booking/RatingsTab.tsx
+++ components/booking/RatingsTab.tsx
@@ -10,25 +10,95 @@ const RatingsTab: React.FC<RatingsTabProps> = ({ ratings }) => {
     <>
       <div className="overflow-y-auto h-[90vh]">
         {
-          <div className="flex flex-row flex-wrap justify-center items-center">
-            {ratings?.map((rating: any, index: any) => (
-              <div key={index} className="w-full  p-4">
-                <div className="bg-white rounded-md shadow-md p-6">
-                  <div className="flex items-center mb-4">
-                    <img
-                      src={rating.imgUrl}
-                      alt="User"
-                      className="w-8 h-8 rounded-full mr-2"
-                    />
-                    <span className="font-bold">{rating.userName}</span>
+          <div className="flex flex-row flex-wrap justify-center items-center mb-2">
+            {ratings?.length !== 0 ? (
+              ratings?.map((rating: any, index: any) => (
+                <>
+                  <div key={index} className="w-full  p-4">
+                    <div className="bg-white rounded-md shadow-md p-6">
+                      <div className="flex items-center mb-4">
+                        <img
+                          src={rating.imgUrl}
+                          alt="User"
+                          className="w-8 h-8 rounded-full mr-2"
+                        />
+                        <span className="font-bold">{rating.userName}</span>
+                      </div>
+                      <div className="mb-4">
+                        <Rate value={rating.rate} disabled />
+                      </div>
+                      <p className="text-sm">{rating.descriptionText}</p>
+                    </div>
                   </div>
-                  <div className="mb-4">
-                    <Rate value={rating.rate} disabled />
+                  <div key={index} className="w-full  p-4">
+                    <div className="bg-white rounded-md shadow-md p-6">
+                      <div className="flex items-center mb-4">
+                        <img
+                          src={rating.imgUrl}
+                          alt="User"
+                          className="w-8 h-8 rounded-full mr-2"
+                        />
+                        <span className="font-bold">{rating.userName}</span>
+                      </div>
+                      <div className="mb-4">
+                        <Rate value={rating.rate} disabled />
+                      </div>
+                      <p className="text-sm">{rating.descriptionText}</p>
+                    </div>
                   </div>
-                  <p className="text-sm">{rating.descriptionText}</p>
-                </div>
-              </div>
-            ))}
+                  <div key={index} className="w-full  p-4">
+                    <div className="bg-white rounded-md shadow-md p-6">
+                      <div className="flex items-center mb-4">
+                        <img
+                          src={rating.imgUrl}
+                          alt="User"
+                          className="w-8 h-8 rounded-full mr-2"
+                        />
+                        <span className="font-bold">{rating.userName}</span>
+                      </div>
+                      <div className="mb-4">
+                        <Rate value={rating.rate} disabled />
+                      </div>
+                      <p className="text-sm">{rating.descriptionText}</p>
+                    </div>
+                  </div>
+                  <div key={index} className="w-full  p-4">
+                    <div className="bg-white rounded-md shadow-md p-6">
+                      <div className="flex items-center mb-4">
+                        <img
+                          src={rating.imgUrl}
+                          alt="User"
+                          className="w-8 h-8 rounded-full mr-2"
+                        />
+                        <span className="font-bold">{rating.userName}</span>
+                      </div>
+                      <div className="mb-4">
+                        <Rate value={rating.rate} disabled />
+                      </div>
+                      <p className="text-sm">{rating.descriptionText}</p>
+                    </div>
+                  </div>
+                  <div key={index} className="w-full  p-4">
+                    <div className="bg-white rounded-md shadow-md p-6">
+                      <div className="flex items-center mb-4">
+                        <img
+                          src={rating.imgUrl}
+                          alt="User"
+                          className="w-8 h-8 rounded-full mr-2"
+                        />
+                        <span className="font-bold">{rating.userName}</span>
+                      </div>
+                      <div className="mb-4">
+                        <Rate value={rating.rate} disabled />
+                      </div>
+                      <p className="text-sm">{rating.descriptionText}</p>
+                    </div>
+                  </div>
+                </>
+              ))
+            ) : (
+              <span className="text-gray-200 my-2">no ratings yet</span>
+            )}
           </div>
         }
       </div>
